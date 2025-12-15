bdb42d6f9b5289cf26efd72c45d49e3e05808d54
Lochier <mohitguptadp7@gmail.com>
2025-12-15 17:00:48 +0530

    Changed bcrypt to bcryptjs

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const userModel = require('../models/user-model');
 const productModel = require('../models/product-model');
 const generateToken = require('../utils/generateToken');
